873fa200a142c9d75780406f2c728bd0d6242319
NIHAL <nilufrmw@gmail.com>
2026-09-01 11:27:05 +0530

    Improve comment formatting in mcu_init.c
    Reformat comments for better readability.

--- src/drivers/mcu/mcu_init.c
+++ src/drivers/mcu/mcu_init.c
@@ -7,9 +7,10 @@ void mcu_init(void) {
   it can lead to mcu reset. We will have to constantly feed the dog so to 
   speak to not issue a reset. So turning it off by default is a good practice 
   unless for systems that has to recover from such states (eg: a flight software 
-  for let's say a weather balloon or spacecraft where manual reset by human interaction 
-  is not possible. In some mcus the WDT is on by default. To check if the WDT on mcu (SAMD21G17D) 
-  is on by default, we have to refer the datasheet of the mcu.
+  for let's say a weather balloon or spacecraft where manual reset by human 
+  interaction is not possible. In some mcus the WDT is on by default. 
+  To check if the WDT on mcu (SAMD21G17D) is on by default, we have to refer 
+  the datasheet of the mcu.
   */
   
 }

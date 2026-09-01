c8508a985fdfd067f80007a6a96aa72a31a8d3b4
NIHAL <nilufrmw@gmail.com>
2026-09-01 10:27:01 +0530

    Update mcu_init.c

--- src/drivers/mcu/mcu_init.c
+++ src/drivers/mcu/mcu_init.c
@@ -7,6 +7,9 @@ void mcu_init(void) {
   it can lead to mcu reset. So turning it off by default is a good practice
   and for systems that has to recover from such states, it's used for example
   a flight software for let's say a weather balloon or spacecraft where manual
-  reset by human interaction is not possible
+  reset by human interaction is not possible. In some mcus the WDT is on by default.
+  To check if the mcu (SAMD21G17D) in the development board (Curiosity Nano Board)
+  is on by default, we have to refer the datasheet.
   */
+  
 }

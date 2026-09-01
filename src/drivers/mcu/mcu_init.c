98b52def993d956906517e7fc36e2b394460e8ca
NIHAL <nilufrmw@gmail.com>
2026-09-01 11:49:36 +0530

    Update mcu_init.c

--- src/drivers/mcu/mcu_init.c
+++ src/drivers/mcu/mcu_init.c
@@ -11,6 +11,11 @@ void mcu_init(void) {
   interaction is not possible. In some mcus the WDT is on by default. 
   To check if the WDT on mcu (SAMD21G17D) is on by default, we have to refer 
   the datasheet of the mcu.
-  */
+
+  Datasheet says - after a power-on reset, some registers will be loaded with
+  initial values from the NVM User Row and suggest to refer to NVM User Row Mapping
+  for more details. Bit 25 (WDT Enable) is 0 by default. So we don't have to
+  stop the watchdog manually.
+  */ 
   
 }

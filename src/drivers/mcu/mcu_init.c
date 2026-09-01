7a9310f08a807a6659994bcae7ba0c28f84ba96a
NIHAL <nilufrmw@gmail.com>
2026-09-01 11:23:41 +0530

    Update mcu_init.c

--- src/drivers/mcu/mcu_init.c
+++ src/drivers/mcu/mcu_init.c
@@ -4,7 +4,8 @@ void mcu_init(void) {
   /*
   The Watchdog Timer (WDT) is a mechanism to monitor 
   the mcu state. If the WDT is on by default and timeout period is small,
-  it can lead to mcu reset. So turning it off by default is a good practice
+  it can lead to mcu reset. We will have to constantly feed the dog so to 
+  speak to not issue a reset. So turning it off by default is a good practice
   and for systems that has to recover from such states, it's used for example
   a flight software for let's say a weather balloon or spacecraft where manual
   reset by human interaction is not possible. In some mcus the WDT is on by default.
